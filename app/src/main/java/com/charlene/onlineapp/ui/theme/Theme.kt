955c6553f75d253635eabe1d7c1cd515f90d42f3
KASUDI780 <kasudicharlene@gmail.com>
2025-05-05 14:57:54 +0300

    adjustment

--- app/src/main/java/com/charlene/onlineapp/ui/theme/Theme.kt
+++ app/src/main/java/com/charlene/onlineapp/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.charlene.onlineapp.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
